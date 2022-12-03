f17d1868b9d92bf9096e25b468fb51e6d98af0db
Dawid Krzoska <dawidwilk19999@gmail.com>
2022-12-03 02:25:52 +0100

    searchBar

--- frontend-linderin/src/pages/landingPage.js
+++ frontend-linderin/src/pages/landingPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "reactstrap";
 import LoginBar from "../components/LoginBar";
+import SearchBar from "../components/SearchBar";
 
 class LandingPage extends React.Component {
   render() {
     return (
       <Container fluid>
         <LoginBar />
+				<SearchBar />
       </Container>
     );
   }
