3a337ed412e036f23c141abe0d5d59766531dee0
Dawid Krzoska <dawidwilk19999@gmail.com>
2022-12-03 01:19:11 +0100

    add landing page as default

---
+++ frontend-linderin/src/pages/landingPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Container,
+  Col,
+  Row,
+  CardImg,
+  Input,
+  Toast,
+  ToastBody,
+  ToastHeader,
+} from "reactstrap";
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+					<Col
+            xl="10"
+          >
+          </Col>
+          <Col
+            className="bg-light border"
+            xl="2"
+          >
+            Jakis test
+          </Col>
+        </Row>
+        <Row></Row>
+      </Container>
+    );
+  }
+}
+
+export default LandingPage;
